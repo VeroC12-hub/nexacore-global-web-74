4cb7ea9b1ba181892b1f7a8fa434c84155ae06c3
VeroC12-hub <godwinocloo21@gmail.com>
2025-08-02 05:07:37 +0000

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -20,7 +20,7 @@ import { useEffect } from 'react';
 const About = () => {
   // Scroll to top when component mounts
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo(1, 1);
   }, []);
   const values = [
     {
