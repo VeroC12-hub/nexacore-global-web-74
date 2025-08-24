bbe2ef5b1cc9d92be17c40db40301acd23afbd71
VeroC12-hub <godwinocloo21@gmail.com>
2025-08-24 23:03:45 +0000

    Adjust company logo styles in PDF generation

--- api/quotes/[id]/pdf.ts
+++ api/quotes/[id]/pdf.ts
@@ -194,8 +194,8 @@ function generateComprehensivePDF(quote: any) {
         }
         
         .company-logo {
-          font-size: 28px;
-          font-weight: 900;
+          font-size: 12px;
+          font-weight: 90;
           letter-spacing: -1px;
           margin-bottom: 5px;
         }
